74365af2381e5bfb671f4a3d7430f16819ac473c
Hammouti Ali <ali.hammouti@epsi.fr>
2022-02-22 00:07:15 +0100

    config : eslint configuration

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,23 +1,31 @@
-function photographerFactory(data) {
+//@ts-check
+const PhotographerFactory = data => {
 
     const { name, portrait } = data;
+
+    /**
+     * Dynamic path of the picture  
+     * @type {string}
+     */
     const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement('article');
+    const getUserCardDOM = () => {
+        const article = document.createElement("article");
 
-        const link = document.createElement('a')
+        const link = document.createElement("a");
         link.setAttribute("href", "");
 
-        const img = document.createElement('img');
+        const img = document.createElement("img");
         img.setAttribute("src", picture);
 
-        const h2 = document.createElement('h2');
+        const h2 = document.createElement("h2");
         h2.textContent = name;
         article.appendChild(link);
         link.appendChild(img);
         link.appendChild(h2);
+
         return (article);
-    }
-    return { name, picture, getUserCardDOM }
-}
+    };
+    return { name, picture, getUserCardDOM };
+};
+export default PhotographerFactory;
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,3 +1,5 @@
+import PhotographerFactory from "../factories/photographer.js";
+
 async function getPhotographers() {
     // Penser à remplacer par les données récupérées dans le json
     const photographers = [
@@ -19,27 +21,28 @@ async function getPhotographers() {
             "price": 500,
             "portrait": "account.png"
         },
-    ]
+    ];
+
     // et bien retourner le tableau photographers seulement une fois
     return ({
         photographers: [...photographers, ...photographers, ...photographers]
-    })
+    });
 }
 
 async function displayData(photographers) {
     const photographersSection = document.querySelector(".photographer_section");
 
     photographers.forEach((photographer) => {
-        const photographerModel = photographerFactory(photographer);
+        const photographerModel = PhotographerFactory(photographer);
         const userCardDOM = photographerModel.getUserCardDOM();
         photographersSection.appendChild(userCardDOM);
     });
-};
+}
 
 async function init() {
     // Récupère les datas des photographes
     const { photographers } = await getPhotographers();
     displayData(photographers);
-};
+}
 
 init();
