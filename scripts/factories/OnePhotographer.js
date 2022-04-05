00a6c71479b63123f439b28c7a5288803d68949a
Hammouti Ali <ali.hammouti@epsi.fr>
2022-04-05 13:54:46 +0200

    fix : error wcag accessibility on the h2 title to contact modal

--- scripts/factories/OnePhotographer.js
+++ scripts/factories/OnePhotographer.js
@@ -92,7 +92,6 @@ export default class OnePhotographer {
 
         const firstFocusableElement = modal.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
         const focusableContent = modal.querySelectorAll(focusableElements);
-        const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
 
         let isTabPressed = e.key === "Tab" || e.keyCode === 9;
 
